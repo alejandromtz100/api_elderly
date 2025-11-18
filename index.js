5583fbdf3880b26295bf189284dd34ded96b7312
alejandromtz100 <alejandro.martinez.22s@utzmg.edu.mx>
2025-11-18 10:43:19 -0600

    Suscribcion Segundo intento

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db");
+
 const app = express();
-const usuarios = require('./src/routes/usuario'); // Rutas para usuarios
-const backupRoutes = require("./src/routes/backup");
-const adulto = require('./src/routes/adulto'); // Rutas para adultos
-const suscripcionRoutes = require("./src/routes/suscripcion");
 
-
+// Rutas
+const usuarios = require("./src/routes/usuario");  
+const backup = require("./src/routes/backup"); 
+const adulto = require("./src/routes/adulto");
+const suscripcion = require("./src/routes/suscripcion"); 
 
 // Conectar a MongoDB
 connectDB();
@@ -16,13 +17,11 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-
-// Rutas
-app.use('/api', usuarios);
-app.use("/api", backupRoutes);
-app.use('/api', adulto);
-app.use("/api", suscripcionRoutes);
-
+// Usar rutas
+app.use("/api", usuarios);
+app.use("/api", backup);
+app.use("/api", adulto);
+app.use("/api/notificaciones", suscripcion);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
--- src/models/Musuario.js
+++ src/models/Musuario.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UsuarioSchema = new mongoose.Schema({
   usuario: { type: String, required: true, unique: true },
@@ -6,13 +6,13 @@ const UsuarioSchema = new mongoose.Schema({
   contra: { type: String, required: true },
   telefono: { type: String, required: true },
   roluser: { type: String, required: true },
-  tokenPermanente: [{ type: String }],
-  adultos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Adulto' }],
 
-  // 🔹 Nuevos campos para notificaciones
+  tokenPermanente: [{ type: String }],
+  adultos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Adulto" }],
+
   suscrito: { type: Boolean, default: false },
-  subscriptionToken: { type: Object, default: null }, // token del navegador para push notifications
-  puedeRecibir: { type: Boolean, default: false } // admin decide si puede recibir notificaciones
+  subscriptionToken: { type: Object, default: null },
+  puedeRecibir: { type: Boolean, default: false }
 });
 
-module.exports = mongoose.model('Usuario', UsuarioSchema);
+module.exports = mongoose.model("Usuario", UsuarioSchema);
